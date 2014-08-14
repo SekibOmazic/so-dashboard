e122cb806e2f08c65ac8157ad49bae22b98d4c10
Sekib Omazic <sekib.omazic@comsysto.com>
2014-08-14 22:51:34 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 so-dashboard
 ============
+
+This is just a fork of the awesome project <a href="https://github.com/ManifestWebDesign/angular-gridster">angular-gridster</a>. So go there if you want to read the documentation.
+
+This fork removes jQuery and jQuery-UI dependencies and adds custom Drag 'n' Resize implementation. 
+
+I needed it for my other project and couldn't wait it to be done by angular-gridster team. 
+
+
+Tested with FF, Chrome and Safari on OSX.  
+
+
+## TO DO
+
+Add an option handle to gridster-item (like jQuery-UI)
---
+++ test/spec/gridster-directive.js
@@ -0,0 +1,115 @@
+'use strict';
+
+describe('gridster directive', function() {
+
+	beforeEach(module('gridster'));
+
+	var $scope;
+	var GridsterCtrl;
+	var $el;
+	var startCount;
+	var resizeCount;
+	var stopCount;
+
+	var dragHelper = function(el, dx, dy) {
+		el.simulate('mouseover').simulate('drag', {
+			moves: 1,
+			dx: dx,
+			dy: dy
+		});
+	};
+
+	beforeEach(inject(function($rootScope, $compile) {
+		$scope = $rootScope.$new();
+		startCount = resizeCount = stopCount = 0;
+
+		$scope.opts = {
+			minRows: 3,
+			resizable: {
+				enabled: true,
+				handles: 'n, e, s, w, se, sw',
+				start: function() {
+					startCount++;
+				},
+				resize: function() {
+					resizeCount++;
+				},
+				stop: function() {
+					stopCount++;
+				}
+			}
+		};
+
+		$scope.dashboard = {
+			widgets: [{
+				id: 1,
+				row: 0,
+				col: 0,
+				sizeX: 1,
+				sizeY: 1
+			}, {
+				id: 2,
+				row: 0,
+				col: 3,
+				sizeX: 2,
+				sizeY: 1
+			}, {
+				id: 3,
+				row: 1,
+				col: 3,
+				sizeX: 2,
+				sizeY: 2
+			}]
+		};
+
+		$el = angular.element('<div gridster="opts" style="width: 1000px;">' +
+			'<ul><li gridster-item="widget" ng-repeat="widget in dashboard.widgets">' +
+			'</div>');
+
+		$el.appendTo(document.body); // append to body so jquery-simulate works
+
+		$compile($el)($scope);
+		$scope.$digest();
+
+		GridsterCtrl = $el.controller('gridster');
+	}));
+
+
+	it('should add a class of gridster', function() {
+		expect($el.hasClass('gridster')).toBe(true);
+	});
+
+	it('should override options', function() {
+		expect(GridsterCtrl.minRows).toBe($scope.opts.minRows);
+	});
+
+	it('should add widgets to DOM', function() {
+		expect($el.find('li').length).toBe($scope.dashboard.widgets.length);
+	});
+
+	it('should initialize resizable', function() {
+		var $widget = $el.find('li:first-child');
+		expect($widget.hasClass('ui-resizable')).toBe(true);
+		expect($widget.find('.ui-resizable-e').length).toBe(1);
+	});
+
+	it('should update widget dimensions on resize & trigger custom resize events', function() {
+		var $widget = $el.find('li:first-child');
+		var handle = $widget.find('.ui-resizable-e');
+
+		expect($widget.width()).toBe(155);
+		expect($scope.dashboard.widgets[0].sizeX).toBe(1);
+		expect(startCount).toBe(0);
+		expect(resizeCount).toBe(0);
+		expect(stopCount).toBe(0);
+
+		dragHelper(handle, 50); // should resize to next width step
+
+		expect($widget.width()).toBe(320);
+		expect($scope.dashboard.widgets[0].sizeX).toBe(2);
+		expect(startCount).toBe(1);
+		expect(resizeCount).toBe(1);
+		expect(stopCount).toBe(1);
+	});
+
+});
